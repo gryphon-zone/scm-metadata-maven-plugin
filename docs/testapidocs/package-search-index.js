c5d93fd1197149bc03b21ed1669811b326fa353f
Tyrol <tyrol@gryphon.zone>
2019-11-11 00:16:03 -0600

    Remove site from master branch

--- docs/testapidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"zone.gryphon.maven.plugins.scm"},{"l":"zone.gryphon.maven.plugins.scm.git"},{"l":"zone.gryphon.maven.plugins.scm.model"},{"l":"zone.gryphon.maven.plugins.scm.testing"}]
